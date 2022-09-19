384f7eb113ae511d749b2de4c8dbae1d062f203f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-19 22:48:59 +0530

    created AtmRepository class

--- src/main/java/com/bank/repository/AtmRepository.java
+++ src/main/java/com/bank/repository/AtmRepository.java
@@ -1,19 +1,4 @@
 package com.bank.repository;
 
-import java.sql.Connection;
-
 public class AtmRepository {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String URL = "jdbc:mysql://localhost:3306/bank";
-
-    private Connection connection;
-
-    public AtmRepository() {
-        this.connection = null;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
 }
--- src/main/java/com/bank/service/DatabaseService.java
+++ src/main/java/com/bank/service/DatabaseService.java
@@ -1,4 +1,23 @@
 package com.bank.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/bank";
+
+    private Connection connection;
+
+    public DatabaseService() {
+        this.connection = null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void printConnect(){
+
+    }
 }
