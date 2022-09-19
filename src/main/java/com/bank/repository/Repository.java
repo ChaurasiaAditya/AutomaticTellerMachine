ead4ccfd27b429408b96b03a02d9928620d55ca6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-19 22:55:32 +0530

    added the method signatures for the Repository interface

--- src/main/java/com/bank/repository/Repository.java
+++ src/main/java/com/bank/repository/Repository.java
@@ -1,4 +1,15 @@
 package com.bank.repository;
 
+import java.sql.Connection;
+
 public interface Repository {
+
+    void getAll(Connection connection);
+    void searchByAccountNumber (Connection connection, int accountNumber);
+    void updateFirstName(Connection connection, int firstName, int accountNumber);
+    void updateLastName(Connection connection, int lastName, int accountNumber);
+    void changeAtmPin(Connection connection, int newAtmPin, int oldAtmPin, int accountNumber);
+    void withdrawCash(Connection connection, int atmPin, double cashToWithdraw);
+    void depositCash(Connection connection, int atmPin, double cashToDeposit);
+    void transferMoney(Connection connection, int atmPin, double cashToTransfer);
 }
