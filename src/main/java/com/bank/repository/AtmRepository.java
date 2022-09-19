c3627e45dbbbd15d9519812b6ba18fd28ba99f47
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-19 22:58:30 +0530

    made the AtmRepository implement Repository and implemented all the methods

--- src/main/java/com/bank/repository/AtmRepository.java
+++ src/main/java/com/bank/repository/AtmRepository.java
@@ -1,4 +1,48 @@
 package com.bank.repository;
 
-public class AtmRepository {
+import com.bank.model.ATM;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class AtmRepository implements Repository<ATM> {
+    @Override
+    public List<ATM> getAll(Connection connection) {
+        return null;
+    }
+
+    @Override
+    public ATM searchByAccountNumber(Connection connection, int accountNumber) {
+        return null;
+    }
+
+    @Override
+    public void updateFirstName(Connection connection, int firstName, int accountNumber) {
+
+    }
+
+    @Override
+    public void updateLastName(Connection connection, int lastName, int accountNumber) {
+
+    }
+
+    @Override
+    public void changeAtmPin(Connection connection, int newAtmPin, int oldAtmPin, int accountNumber) {
+
+    }
+
+    @Override
+    public double withdrawCash(Connection connection, int atmPin, double cashToWithdraw) {
+        return 0;
+    }
+
+    @Override
+    public double depositCash(Connection connection, int atmPin, double cashToDeposit) {
+        return 0;
+    }
+
+    @Override
+    public double transferMoney(Connection connection, int atmPin, double cashToTransfer) {
+        return 0;
+    }
 }
