521ccdcfd12a77843113ebf8fd50a85bd328664d
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-19 22:43:00 +0530

    added the no args and parametrised constructor for ATM

--- src/main/java/com/bank/model/ATM.java
+++ src/main/java/com/bank/model/ATM.java
@@ -5,4 +5,14 @@ public class ATM {
     private String lastName;
     private int accountNumber;
     private int atmPin;
+
+    public ATM() {
+    }
+
+    public ATM(String firstName, String lastName, int accountNumber, int atmPin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+        this.atmPin = atmPin;
+    }
 }
