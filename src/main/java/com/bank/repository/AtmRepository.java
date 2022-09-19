744bbe78dc67a4e80e014ffdaa4c41122ab0cfae
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-19 22:42:19 +0530

    added fields in AtmRepository class

--- src/main/java/com/bank/repository/AtmRepository.java
+++ src/main/java/com/bank/repository/AtmRepository.java
@@ -1,4 +1,7 @@
 package com.bank.repository;
 
 public class AtmRepository {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/bank";
 }
